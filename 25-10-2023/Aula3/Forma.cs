885f2a3ed9f5a54166b29336c99386b228fc5ff1
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2023-10-30 18:00:16 -0400

    aula 25/10

---
+++ 25-10-2023/Aula3/Forma.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula3
+{
+    public class Forma
+    {
+        public static string Nome { private get; set; }
+        public string RetornaNome()
+        {
+            return "Forma de bolo";
+        }
+
+        private int RetornaTamanho()
+        {
+            return 10;
+        }
+
+        public string Xpto()
+        {
+            return "";
+        }
+        public string Xpto(string x)
+        {
+            return x;
+        }
+        public string Xpto(string x, int i, string y = "00000")
+        {
+            var j = 1;
+            return x + 1;
+        }
+    }
+}
---
+++ 25-10-2023/Aula3/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Text;
+
+namespace Aula3
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = ( Forma.Nome = Forma.Nome + Forma.Nome "sd") 
+            int resultado; ; // por valor => Stack
+             string str = "ss"; //por referencia
+
+
+            int valorA = 10;
+            int valorB = valorA;
+
+            string x = "aaaa"; //o que define um escopo de codigo é a {} =chave; o que tive fora disso não temos acesso;
+            
+            {
+                int y = 10;
+                Console.WriteLine(y);
+            }
+
+           
+
+            AlterarVariavelPorValor(valorA);
+            Console.WriteLine("Alterado por valor "+ valorA);
+
+            AlterarVariavelPorReferencia(ref valorB);
+            Console.WriteLine("Alterado por referencia " + valorB);
+
+            Stream s = new MemoryStream();
+
+            //byte[] bytes = Encoding.UTF8.GetBytes("isaias barros");
+
+
+            // Array.ForEach(bytes, e =>
+            //{
+
+            //    File.AppendAllText(@"C:\bb.txt", e.ToString());
+            //});
+
+            var file = File.ReadAllLines(@"C:\bb.txt").ToList();
+
+            file = file.Select(e =>
+            {
+                e = e.Replace("isaias", "teste");
+                return e;
+
+            }).ToList();
+
+            File.WriteAllLines(@"C:\bb.txt", file);
+
+            var ss = teste();
+
+        
+           
+        }
+
+        static void AlterarVariavelPorReferencia(ref int num )
+        {
+            num = 50;
+        }
+
+        static void AlterarVariavelPorValor(int ? num = 0)
+        {
+            num = 100;
+        }
+
+        public static (string t, int d) sss()
+        {
+           
+            return ("isaias", 2);
+        }
+
+        public static Tuple<string, int> teste()
+        {
+            return new Tuple<string, int>("barros", 1);
+        }
+    }
+}
