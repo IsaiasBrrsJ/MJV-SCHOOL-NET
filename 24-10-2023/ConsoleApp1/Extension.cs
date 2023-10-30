4dadc32cf7d51bc6c241eef426218a9dec368fe0
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2023-10-30 17:59:48 -0400

    Aula 24/10

---
+++ 24-10-2023/ConsoleApp1/Extension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MinhaExcecaoPersonalizada : Exception
+    {
+        public MinhaExcecaoPersonalizada()
+        {
+        }
+
+        public MinhaExcecaoPersonalizada(string message)
+            : base(message)
+        {
+        }
+
+        public MinhaExcecaoPersonalizada(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ 24-10-2023/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Beep();
+            Int16 i16 = 9999;
+            Int32 i32 = 999999999;
+            Int64 i64 = 999999999999999999;
+            bool? ativo;
+            int? ii;
+            Int16? hh;
+            string ss;
+
+            var idade1 = 101;
+            dynamic teste = idade1 + "2";
+            Object iade4 = 10;// no .net tudo é Object
+
+            var xx = Console.Read();
+            var yy = Console.ReadKey();
+
+            List<int> nn = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7,    8, 9, 10,   11, 12, 13, 14, 15, 16, 17, 18, 19
+
+            };
+
+            IQueryable<List<int>> l = from c in nn
+                                      select c.roli;
+
+
+
+
+
+
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(xx);
+            Console.WriteLine(yy);
+
+            Console.WriteLine("Tipo da variavel idade1: " + idade1.GetType());
+
+
+            Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+
+            Console.WriteLine("Digite sua idade: ");
+            string idade = Console.ReadLine();
+
+            Console.WriteLine
+           ($"Nome digitado: {nome} Idade: {idade}");
+        }
+        // C# coompila para IL ()
+       
+    }
+  
+}
