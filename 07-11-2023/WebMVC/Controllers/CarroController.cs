245da8137b07d2daa743c5a5f2a898f5f938d5d7
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2023-11-07 20:13:00 -0400

    Atividade MVC

---
+++ 07-11-2023/WebMVC/Controllers/CarroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVC.Controllers
+{
+    public class CarroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
